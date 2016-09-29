983922f76d6d7a0ca66c89d9a87ed33915be6087
Ala <alaroshdieh@gmail.com>
2016-09-29 13:58:55 -0400

    added mobile folder, and mobile scss file for mobile scssification! Also added a bunny picture to thankyou php

--- page-registration-thankyou.php
+++ page-registration-thankyou.php
@@ -7,10 +7,17 @@
  * @since Twenty Fourteen 1.0
  */
 get_header();
-
 ?>
 
 <h1> Thank You for registering with Magic Languages! </h1>
 <p> Thank you for registering with Magic Languages! You will recieve an email
 	confirmation with an order reciept and further information about your
 	program and login information for further online resources</p>
+<img src="images/bunny/black_bunny.png"
+
+
+<?php
+get_footer();
+
+
+?>
