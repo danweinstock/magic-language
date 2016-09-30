4d1958423fca9f9cf47e5c6a5e442e63b90464b7
danweinstock <dan.weinstock@yahoo.com>
2016-09-30 10:39:19 -0400

    removed screen width from body

--- template-parts/content-page-programs.php
+++ template-parts/content-page-programs.php
@@ -52,75 +52,32 @@
 	<p>If you are a school, kindergarten, daycare or any other center for child education, Magic Languages can help your students learn French, Spanish and Mandarin. Our teachers are available for before-after school programs and for lessons integrated in your regular curriculum</p>
 </div>
 <div class="programs-accordion">
-	<ul id="accordion">
-  <li><span class="large-6">This is FAQ 1</span>
-    <div>This is the answer to the FAQ 1. Human is washing you why halp oh the horror flee scratch hiss bite scratch at the door then walk away drink water out of the faucet, yet jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed chirp at birds.  
+
+<ul class="accordion" data-accordion>
+	<li class="accordion-item">
+		<a href="#" class="accordion-title">FAQ 1</a>
+		<div class="accordion-content " data-tab-content>
+      This is the answer to the FAQ 1. Human is washing you why halp oh the horror flee scratch hiss bite scratch at the door then walk away drink water out of the faucet, yet jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed chirp at birds. 
     </div>
-  </li>
-  <li><span class="large-6">This is FAQ 2</span>
-    <div>  This is the answer to FAQ 2. Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff. I like big cats and i can not lie inspect anything brought into the house, and lick butt.
-    </div>
-  </li>
-  <li><span class="large-6">This is FAQ 3</span>
-    <div>  This is the answer to FAQ 3. Wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again need to chase tail russian blue or sleep in the bathroom sink, hunt anything that moves. Pelt around the house and up and down stairs chasing phantoms sweet beast paw at beetle and eat it before it gets away run outside as soon as door open.
-    </div>
-  </li>
-  
-  <li><span >This is FAQ 4</span>
-    <div>  This is the answer to FAQ 4. Find empty spot in cupboard and sleep all day spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce. White cat sleeps on a black shirt sit on the laptop. Poop on grasses wake up human for food at 4am. 
-      </div>     
-  </li>
-  
+	</li>
+	<li class="accordion-item">
+		<a href="#" class="accordion-title">This is FAQ 2</a>
+		<div class="accordion-content" data-tab-content>
+			This is the answer to FAQ 2. Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff. I like big cats and i can not lie inspect anything brought into the house, and lick butt.
+		</div>
+	</li>
+	<li class="accordion-item">
+		<a href="#" class="accordion-title">This is FAQ 3</a>
+		<div class="accordion-content" data-tab-content>
+			This is the answer to FAQ 3. Wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again need to chase tail russian blue or sleep in the bathroom sink, hunt anything that moves.
+		</div>
+	</li>
+	<li class="accordion-item">
+		<a href="#" class="accordion-title">This is FAQ 4</a>
+		<div class="accordion-content" data-tab-content>
+			This is the answer to FAQ 4. Find empty spot in cupboard and sleep all day spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce. White cat sleeps on a black shirt sit on the laptop. Poop on grasses wake up human for food at 4am. 
+		</div>
+	</li>
+
 </ul>
 </div>
-
-
-<script>
-	jQuery( document ).ready(function() {
-    jQuery("#accordion > li > span").click(function() {
-    jQuery(this).closest('li').siblings().find('span').removeClass('active').next('div').slideUp(250);
-    jQuery(this).toggleClass("active").next('div').slideToggle(250);
-		});
-	});
-</script>
-<style>
-	#accordion {
-	list-style: none;
-	padding: 2px;
-}
-#accordion > li {
-	display: block;
-	list-style: none;
-}
-#accordion > li > span {
-	display: block;
-	color: #fff;
-	margin: 4px 0;
-	padding: 6px;
-	background: #525252;
-	/*background: url(images/expand_arrow.png) no-repeat 99.5% 6px #525252;*/
-	background-size: 20px;
-	font-weight: normal;
-	cursor: pointer; font-size:16px
-}
-#accordion > li > div {
-	list-style: none;
-	padding: 6px;
-	display: none; overflow:auto
-}
-#accordion > ul li {
-	font-weight: normal;
-	cursor: auto;
-	padding: 0 0 0 7px;
-}
-#accordion a {
-	text-decoration: none;
-}
-#accordion li > span:hover {
-}
-#accordion li > span.active {
-/*	background: url(images/collapse-arrow.png) no-repeat 99.5% 6px #000;*/
-background: #000;
-	background-size: 20px
-}
-</style>
