a7708b134d468931e46b3441f2c5e72bebaef5c0
BrandonPuim <brandon.puim@gmail.com>
2016-09-30 14:29:48 -0400

    location page updated

--- page-location.php
+++ page-location.php
@@ -23,20 +23,16 @@ get_header(); ?>
 
 				get_template_part( 'template-parts/content', 'location-neighbourhood' );
 
+				get_template_part( 'template-parts/content', 'location-neighbourhood-info' );
+
 				// If comments are open or we have at least one comment, load up the comment template.
 
 			?>
 
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
             </div>
-            <div class="">
-                <h4>Start your child on the journey to a new language today.</h4>
-                <a href="https://www.google.ca"> <button class="">Programs</button> </a>
-            </div>
+            
 
-			<?php endwhile; wp_reset_query();?>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- template-parts/content-location-neighbourhood.php
+++ template-parts/content-location-neighbourhood.php
@@ -2,8 +2,11 @@
 <?php $neighbourhood = new WP_Query( array( 'post_type' => 'Neighbourhood','posts_per_page' => 20 ) ); ?>
 
 <?php while ( $neighbourhood->have_posts()){ ?>
-        <?php $neighbourhood->the_post(); ?>
-        <div class="">
-            <?php the_post_thumbnail(); ?>
-        </div>
-<?php } wp_reset_query();?>
+    <?php $neighbourhood->the_post(); ?>
+    <div class="location-part" data-map="<?php the_ID()?>">
+        <button><?php the_post_thumbnail()?></button>
+    </div>
+<?php } ?>
+    <script>
+
+    </script>
