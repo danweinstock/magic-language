3cb06332b90bfe03e648ee1c7acc32e0f5bd5117
Ala <alaroshdieh@gmail.com>
2016-10-04 15:57:48 -0400

    styled program pages, working on registration banner at bottom

--- template-parts/content-page-programs.php
+++ template-parts/content-page-programs.php
@@ -1,59 +1,71 @@
 <h1 class="program-header-image text-center blue-text program-header-text ">Programs</h1>
 <div class="program-title text-center blue-text"><h3>Our Methodology</h3></div>
 <div class="program-subtitle text-center blue-text">One class a week + 15 minutes a day at home</div>
-<div class="program-info-icons">
-	<div class="program-puzzle">
+<div class="program-info-icons flex padding-lrg">
+	<div class="program-puzzle ">
 		<i class="fa fa-puzzle-piece" aria-hidden="true"></i>
 		<p>Children learn through play without realizing it.</p>
 	</div>
-	<div class="program-music">
+	<div class="program-music ">
 		<i class="fa fa-music" aria-hidden="true"></i>
 		<p>Songs help children practice new sounds</p>
 	</div>
-	<div class="program-globe">
+	<div class="program-globe ">
 		<i class="fa fa-globe" aria-hidden="true"></i>
 		<p>Introduction to cultural elements reinforce the learning</p>
 	</div>
-	<div class="program-book">
+	<div class="program-book ">
 		<i class="fa fa-book" aria-hidden="true"></i>
 		<p>Word order and grammar are taught through story</p>
 	</div>
 </div>
-<div class="methodology">
+<div class="methodology text-center">
 	Whether your child is 12 months or 12 years old, Magic Languages has a program to fit.<br> We teach French, Spanish and Mandarin class in a variety of formats with no previous knowledge required.
 </div>
-<div class="programs-in-home">
-	<h3>Learn in your own home</h3>
-	<p>Learn with Magic Languages in the comfort of your own home. For private or small group instruction we offer one lesson per week (or more if you wish). </p>
-	<ul>
-		<li>First child $180, discounts for additional children.</li>
-		<li>Sold in packages of 4 lessons</li>
-		<li>Groups of 1-5 children</li>
-		<li>Specific activities for each age group</li>
-		<li>Access to digital content for home practice</li>
-		<li>A great option for home-schooled children</li>
-	</ul>
-	<a href="#">Register</a>
+<div class="row ">
+	<div class="programs-in-home padding-programs large-6 columns">
+		<h3 class="blue-text">Learn in your own home</h3>
+		<p>Learn with Magic Languages in the comfort of your own home. For private or small group instruction we offer one lesson per week (or more if you wish).</p>
+		<ul>
+			<li>First child $180, discounts for additional children.</li>
+			<li>Sold in packages of 4 lessons</li>
+			<li>Groups of 1-5 children</li>
+			<li>Specific activities for each age group</li>
+			<li>Access to digital content for home practice</li>
+			<li>A great option for home-schooled children</li>
+		</ul>
+		<button class="orange-button">Register</button>
+	</div>
+	<div class="programs-in-school padding-programs large-6 columns">
+		<h3 class="blue-text"> Join a Group Class</h3>
+		<p>Learn on location by joining one of our 10 week group programs. Offered in various neighbourhoods across Toronto. Classes vary based on age and language. </p>
+		<ul>
+			<li>First child $250, discounts for additional children</li>
+			<li>Sold in packages of 10 lessons</li>
+			<li>Maximum of 6 students per class</li>
+			<li>Choice of 6 locations</li>
+			<li>Access to digital content for home practice</li>
+			<li>Specific activities for each group</li>
+		</ul>
+		<button class="orange-button">Register</button>
+		<button class="blue-button">Locations &amp; Schedule</button>
+	</div>
 </div>
-<div class="programs-in-school">
-	Learn on location by joining one of our 10 week group programs. Offered in various neighbourhoods across Toronto. Classes vary based on age and language.
-	<ul>
-		<li>First child $250, discounts for additional children</li>
-		<li>Sold in packages of 10 lessons</li>
-		<li>Maximum of 6 students per class</li>
-		<li>Choice of 6 locations</li>
-		<li>Access to digital content for home practice</li>
-		<li>Specific activities for each group</li>
-	</ul>
-	<a href="#">Register</a>
-	<a href="#">Locations &amp; Schedule</a>
-</div>
-<div class="programs-add-school">
-	<p>If you are a school, kindergarten, daycare or any other center for child education, Magic Languages can help your students learn French, Spanish and Mandarin. Our teachers are available for before-after school programs and for lessons integrated in your regular curriculum</p>
+<div class="row">
+	<div class="programs-add-school large-6 column">
+		<h3 class="blue-text">In-School Programs</h3>
+		<p>If you are a school, kindergarten, daycare or any other center for child education, Magic Languages can help your students learn French, Spanish and Mandarin. Our teachers are available for before-after school programs and for lessons integrated in your regular curriculum</p>
+		<button class="blue-button">Contact Us</button>
+	</div>
+	<div class="large-6 column">
+		<?php
+			echo '<img src="'.get_bloginfo("stylesheet_directory").'/images/bunny/black_bunny.png">'."<br>";
+		?>
+	</div>
 </div>
 
 <div class="programs-accordion">
-
+<h1 class="blue-text txt-center"> FAQs</h1>
 <ul class="accordion" data-accordion>
 	<li class="accordion-item">
 		<a href="#" class="accordion-title">FAQ 1</a>
@@ -83,7 +95,6 @@
 </ul>
 </div>
 <div class="programs-page-register">
-	Start your child on the journey to a new language today.
-
+	<h3 class="txt-center">Start your child on the journey to a new language today.</h3>
 	<a href="#">Register</a>
 </div>
