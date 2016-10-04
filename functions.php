7c1862c8fb9018a45f75e1c72eddc2d2743b0424
danweinstock <dan.weinstock@yahoo.com>
2016-10-04 11:10:26 -0400

    added separate forms for registraton and styling

--- functions.php
+++ functions.php
@@ -117,7 +117,7 @@ wp_enqueue_style( 'magic_language-style', get_stylesheet_uri() );
 	wp_enqueue_script( 'magic_language-custom-scripts', get_template_directory_uri(). '/js/site.js');
 
 	wp_enqueue_script( 'magic_language-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	wp_enqueue_script('foundation-init',get_bloginfo("stylesheet_directory").'/foundation-init.js');
+	wp_enqueue_script('foundation-init',get_bloginfo("stylesheet_directory").'/foundation-6.2.3/js/app.js');
 	wp_enqueue_script('foundation-min-js',get_bloginfo("stylesheet_directory").'/foundation-6.2.3/js/vendor/foundation.min.js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
