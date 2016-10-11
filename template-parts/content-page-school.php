af16e4ec6b49101429c2c81e303fb5078ae58f4d
Ala <alaroshdieh@gmail.com>
2016-10-11 11:26:42 -0400

    adding changes to style.css and build folder

---
+++ template-parts/content-page-school.php
@@ -0,0 +1,30 @@
+<div class="school-div">
+	<p class="text-left">If you are a school, kindergarten, daycare, or any other center for child education and you
+		would like to offer French, Spanish, Mandarin language class, Magic-Languages can help you.
+		Our teachers can teach before- and after-school programs or integrate lessons in you regular
+		curriculum. </p>
+
+	<p class="text-left">Please contact us and benefit from a professional teaching service with a proven methodology
+		directly at your school</p><br>
+
+
+	<div class="row ">
+		<div class="large-6 columns ">
+			<h5 class="blue-text"> <strong>Get in Touch with Us</strong></h5>
+			<form action="#">
+				First and Last Name: <input type="text" name="flname">
+				Job Title:<input type="text" name="jobtitle">
+				Name of your Education Center:<input type="text" name="education">
+				Email:<input type="text" name="email-education">
+				Phone Number:<input type="text" name="number-education">
+				Leave us a Message:<input type="text" class="message-box" name="message-education">
+				<button type="button" class="blue-button">Submit</button>
+			</form>
+		</div>
+		<div class="large-6 columns bunny-school">
+			<?php
+				echo '<img src="'.get_bloginfo("stylesheet_directory").'/images/bunny/black_bunny.png">'."<br>";
+			?>
+		</div>
+	</div>
+</div>
